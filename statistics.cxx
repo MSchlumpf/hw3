772177d550431e9b7ca214d3a0a10e30e2a5863e
Michael Stumpf <Michael.Stumpf@uni-duesseldorf.de>
2015-11-12 09:47:30 +0100

    hw 3 done. but mean and var are always the same.

--- statistics.cxx
+++ statistics.cxx
@@ -1,19 +1,40 @@
+/* Programm statistics changed by
+ * Michael Stumpf
+ */
+
 #include <cmath>
 #include <iostream>
 #include <cstdlib>
 
 using namespace std;
-//--------------------------------------------------
-// plenty of lines here
-//--------------------------------------------------
+
+//creates N random numbers
+void rand(double* p, int N){
+ for (int i=0; i<N; i++){
+  p[i] = rand()/double(RAND_MAX);
+  //cout << p[i] << endl; //uncomment to check random numbers
+ }
+}
+
+//calculates mean value and variance of an array p[N]
+void moments(double* p, int N, double& mean, double& var){
+  for(int i=0; i<N; i++){
+    mean += p[i];
+  }		//loop for mean value
+  mean = mean/double(N);
+  for(int i=0; i<N; i++){
+    var += (p[i]-mean)*(p[i]-mean);
+  }		//loop for variance
+  var = var/double(N);
+}
 
 int main(){
    const int N = 100;
    double p[N];
-   double mean, var;
-
-   // Some lines here....
-
+   double mean=0, var=0;
+   rand(p, N);			//Create random numbers
+   moments(p, N, mean, var);	//calculate mean value and variance of p[N]
+   
    cout << "Mean = " << mean << endl;
    cout << "Variance = " << var << endl;
 
